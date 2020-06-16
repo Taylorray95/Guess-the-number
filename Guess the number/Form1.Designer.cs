1894f8c61074f6b62a96dee3542349f53b65c3df
taylor ray <taylorleavelle@outlook.com>
2020-06-16 12:18:25 -0500

    Final Version

---
+++ Guess the number/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace Guess_the_number
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label_highlow = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AutoSize = true;
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.label_highlow);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.MinimumSize = new System.Drawing.Size(2000, 1200);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(2149, 1337);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label6
+            // 
+            this.label6.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Century Gothic", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(481, 1002);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(23, 32);
+            this.label6.TabIndex = 14;
+            this.label6.Text = " ";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 347);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(23, 32);
+            this.label5.TabIndex = 13;
+            this.label5.Text = " ";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(481, 935);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(23, 32);
+            this.label4.TabIndex = 12;
+            this.label4.Text = " ";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(1154, 301);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 32);
+            this.label3.TabIndex = 11;
+            // 
+            // button3
+            // 
+            this.button3.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Font = new System.Drawing.Font("Century Gothic", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(1071, 621);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(250, 170);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Cheat and Show Random Number";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Century Gothic", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(658, 621);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(250, 170);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Did You Guess Correctly??";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label_highlow
+            // 
+            this.label_highlow.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label_highlow.AutoSize = true;
+            this.label_highlow.Font = new System.Drawing.Font("Century Gothic", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_highlow.Location = new System.Drawing.Point(481, 843);
+            this.label_highlow.Name = "label_highlow";
+            this.label_highlow.Size = new System.Drawing.Size(23, 32);
+            this.label_highlow.TabIndex = 8;
+            this.label_highlow.Text = " ";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(574, 406);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(383, 32);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Enter your First Guess Below.";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(240, 347);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1101, 32);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "I Have A Number Between 1 And 1000 — You\'ll Never Be Able To Guess It Mwahaa!";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.textBox1.Font = new System.Drawing.Font("Century Gothic", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.textBox1.Location = new System.Drawing.Point(723, 547);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 41);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(266, 621);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(250, 170);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Generate New Random Number and Try Again";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(502, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(633, 290);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(2149, 1337);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(2000, 1200);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Can You Guess the Number?";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label_highlow;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Guess the number/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Tab;
+
+namespace Guess_the_number
+{
+    public partial class Form1 : Form
+    {
+        public static int count = 0;
+        private int randomnum;
+        private int users_last_guess;
+        private int usersguess;
+       
+        public Form1()
+        {
+            InitializeComponent();
+            genrandom();
+        }
+        private void genrandom()
+        {
+            Random random = new Random();
+            randomnum = random.Next(1, 1001);
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            label2.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.BackColor = System.Drawing.Color.White;
+            panel1.BackColor = System.Drawing.Color.White;
+            textBox1.Enabled = true;
+            textBox1.Text = "";
+            genrandom();
+            label5.Text = "";
+            label5.Text = "";
+            label4.Text = "";
+            label1.Text = "I Have A Number Between 1 And 1000 — You'll Never Be Able to Guess it Mwahaa!";
+            textBox1.Enabled = true;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {   
+            count++;
+            label6.Text = "Amount of Guesses\n" +
+                "You've Conducted: " + count;
+            label2.Text = "Wrong Again! You'll Never Get it,\n" +
+                          "Ha! But Enter Another Number To Try\n" +
+                          " Again If You Insist....";
+            label5.Text = "";
+            try
+            {   label4.Text = "Blue : Further than your previous guess \n" +
+                                 "Red : Closer than your previous guess";
+                if (Convert.ToInt32(textBox1.Text) == 0 || (Convert.ToInt32(textBox1.Text) > 1000))
+                {
+                    label4.Text = "";
+                    label_highlow.Text = "";
+                    label5.Text = "Hmm.. guess something \n" +
+                        "between 1-1000. Because you Defnitely\n" +
+                        "Just Wasted a Guess!!"; 
+                    label2.Text = "Please enter a number below:"; return;
+                }
+                label4.Text = "Blue : Further than your previous guess \n" +
+                                 "Red : Closer than your previous guess";
+                usersguess = Convert.ToInt32(textBox1.Text);
+                if (usersguess == randomnum)
+                {
+                    panel1.BackColor = System.Drawing.Color.Green;
+                    msg m = new msg();
+                    m.Show();
+                    textBox1.Enabled = false;
+                    label_highlow.Text = "";
+                    label5.Text = "";
+                    label4.Text = "";
+                    label2.Text = "Hey, You actually Got It! Darnit!\n" +
+                                  "Want to Play Again??? No Way You\n" +
+                                  "Can Do it Twice...";
+                    label1.Text = "";
+                    count = 0;
+                }
+                int users_accuracy = Math.Abs(randomnum - usersguess);
+                int users_last_accuracy = Math.Abs(randomnum - users_last_guess);
+                if (users_accuracy < users_last_accuracy)
+                {
+                    textBox1.BackColor = System.Drawing.Color.Red;
+                }
+                if (users_accuracy > users_last_accuracy)
+                {
+                    textBox1.BackColor = System.Drawing.Color.Blue;
+                }
+                if (usersguess > randomnum)
+                {
+                    label_highlow.Text = "Ouch,Too High, Try Again!";
+                }
+                if (usersguess < randomnum)
+                {
+                    label_highlow.Text = "Oops, Too Low!";
+                }
+                users_last_guess = usersguess;
+            }
+            catch (Exception ex)
+            {
+                label5.Text = "Hmm.. Something Seems to Have\n" +
+                    "Went Wrong With Your Input Try again, This\n" +
+                    "Time, Be sure It's A Number And Not Text.\n" +
+                    "You Just Wasted A Guess.";
+                label4.Text = "";
+                label_highlow.Text = "";
+                label2.Text = "Please Enter A Number Below:";
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = randomnum.ToString();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
